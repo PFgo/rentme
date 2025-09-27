b1716f07eb8a3aa6e7d90f3af66fbff16d3b297a
pengfei guo <adz9596@gmail.com>
2025-09-26 22:44:14 -0700

    Build to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rentme/", 
+  base: '/rentme/',          // repo name
+  build: { outDir: 'docs' }, // emit build into docs/
 })
